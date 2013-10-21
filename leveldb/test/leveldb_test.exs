97976cb8380e5776d75903de78f0a99da27559e3
Mike Bild <mike@subkit.io>
2013-10-21 11:51:34 +0200

    test setup

--- leveldb/test/leveldb_test.exs
+++ leveldb/test/leveldb_test.exs
@@ -1,21 +1,28 @@
 defmodule LeveldbTest do
   use ExUnit.Case
 
+
+  setup do
+    Leveldb.store
+    :ok
+  end
+
   test "key 'a' should be value 'first'" do
-  	Leveldb.store
   	{:ok, data} = Leveldb.get <<"a">>
     assert data == "first"
   end
 
   test "all should return 'first, second, third'" do
-  	Leveldb.store
   	data = Leveldb.all
     assert data == [{"a", "first"}, {"b", "second"}, {"c", "third"}]
   end
 
   test "all with start key should return 'second, third'" do
-    Leveldb.store
     data = Leveldb.all <<"b">>
     assert data == [{"b", "second"}, {"c", "third"}]
+  end
+
+  teardown do
+    :ok
   end
 end
